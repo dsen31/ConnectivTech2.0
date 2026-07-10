678c38f6a4a3d70352134b9cfeb659c598534a47
Dustin Senor <dustin.senor@gmail.com>
2026-07-10 11:08:17 -0400

    Add BCC to sender on outgoing emails

--- app/actions/email.ts
+++ app/actions/email.ts
@@ -274,6 +274,7 @@ export async function sendCampaignStep(campaignId: string): Promise<{
         from: FROM,
         to: lead.email,
         replyTo: ["dustin@actoradvisory.com"],
+        bcc: "dustin@actoradvisory.com",
         subject,
         html,
         text,
